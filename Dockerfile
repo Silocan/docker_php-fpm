e49f584d5faba91dfa71936b80f05f2c4ccd024d
Nicolas F <nicolas@unid-consulting.fr>
2020-10-29 07:39:19 +0100

    initial commit with php7.4-fpm

---
+++ Dockerfile
@@ -0,0 +1,54 @@
+FROM php:7.4-fpm
+LABEL maintainer="nicolas@unid-consulting.fr"
+
+
+RUN set -ex; \
+    \
+    apt-get -yqq update; \
+    apt-get -yqq install \
+    libjpeg62-turbo-dev \
+    libpng-dev \
+    libfreetype6-dev \
+    libxml2-dev \
+    libicu-dev \
+    msmtp \
+    curl \
+    git \
+    zip \
+    unzip \
+    libzip-dev \
+    vim \
+    libxml2-dev \
+    libcurl3-dev \
+    libonig-dev \
+    mailutils \
+    ; \
+    rm -rf /var/lib/apt/lists/*; \
+    \    
+    docker-php-ext-configure mysqli; \
+    docker-php-ext-configure gd --with-freetype --with-jpeg; \
+    docker-php-ext-install intl opcache pdo pdo_mysql mbstring gd zip bcmath xml json mysqli curl calendar; \
+    pecl install mongodb && echo "extension=mongodb.so" >> $PHP_INI_DIR/conf.d/mongodb.ini
+
+# Composer 
+RUN set -ex; \     
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer; \     
+    chmod +x /usr/local/bin/composer
+
+RUN version=$(php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;") \
+    && curl -A "Docker" -o /tmp/blackfire-probe.tar.gz -D - -L -s https://blackfire.io/api/v1/releases/probe/php/linux/amd64/$version \
+    && mkdir -p /tmp/blackfire \
+    && tar zxpf /tmp/blackfire-probe.tar.gz -C /tmp/blackfire \
+    && mv /tmp/blackfire/blackfire-*.so $(php -r "echo ini_get('extension_dir');")/blackfire.so \
+    && printf "extension=blackfire.so\nblackfire.agent_socket=tcp://blackfire:8707\n" > $PHP_INI_DIR/conf.d/blackfire.ini \
+    && rm -rf /tmp/blackfire /tmp/blackfire-probe.tar.gz
+
+COPY docker/msmtp/msmtprc /etc/msmtprc
+COPY docker/docker-entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+WORKDIR /var/www
+
+ENTRYPOINT ["/entrypoint.sh"]
+
+CMD ["php-fpm", "-F"]
---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+
+if [ "$USER" = "root" ]; then
+
+    # set localtime
+    ln -sf /usr/share/zoneinfo/$LOCALTIME /etc/localtime
+fi
+
+#
+# functions
+
+function set_conf {
+    echo ''>$2; IFSO=$IFS; IFS=$(echo -en "\n\b")
+    for c in `printenv|grep $1`; do echo "`echo $c|cut -d "=" -f1|awk -F"$1" '{print $2}'` $3 `echo $c|cut -d "=" -f2`" >> $2; done;
+    IFS=$IFSO
+}
+
+#
+# PHP
+
+echo "date.timezone = \"${LOCALTIME}\"" >> $PHP_INI_DIR/conf.d/00-default.ini
+set_conf "PHP__" "$PHP_INI_DIR/conf.d/40-user.ini" "="
+
+chmod 777 -Rf /var/www
+
+echo "Check composer";
+if [[ -f /var/www/composer.json && $COMPOSER_INSTALL -eq 1 ]]; then
+    echo "Composer install";
+    cd /var/www
+    composer install --prefer-dist --no-progress --no-suggest --no-interaction --optimize-autoloader
+fi
+
+
+echo -e "GENERATE_API_KEY = ${GENERATE_API_KEY}\n"
+echo -e "jwt_passhrase = ${jwt_passhrase}\n"
+if [[ $GENERATE_API_KEY -eq 1 && -z "$jwt_passphrase" ]]; then
+    echo "Generation de la clef d'API"
+    cd /var/www
+    mkdir -p var/jwt
+    echo "$jwt_passhrase" | openssl genpkey -out var/jwt/private.pem -pass stdin -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
+    echo "$jwt_passhrase" | openssl pkey -in var/jwt/private.pem -passin stdin -out var/jwt/public.pem -pubout
+fi
+
+if [[ -f /var/www/bin/console && -f /var/www/.env && $DOCTRINE_LOAD -eq 1 ]]; then
+    echo "--- Doctrine initialisation";
+    cd /var/www
+
+    echo ".. Création de la base"
+    php bin/console doctrine:database:create --if-not-exists --no-interaction
+    echo ".. Mise à jour de la structure"
+    php bin/console doctrine:migration:migrate  --allow-no-migration --no-interaction
+    #echo ".. Chargement des fixtures"
+    #php bin/console doctrine:fixtures:load --append
+fi
+
+exec "$@"
